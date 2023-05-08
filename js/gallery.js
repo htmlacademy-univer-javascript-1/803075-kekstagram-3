40a7f9d48d9a7eb4a493ab14a294719923bd79ba
the-romantic-dev <ahxplay@gmail.com>
2023-05-08 13:35:44 +0300

    Доработка функционала Настроил получение и отправку данных на сервер. Убрал лишние файлы Изменил валидацию полей формы на более лакончиное Кучу всего реструктуризировал и рефакторил.

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,37 @@
-import {generatePhotos} from './data.js';
+import { getGalleryFromServer } from './server.js';
+import { showError } from './errors.js';
 
-function createPictureElement(data, element) {
+export const fillPicturesList =  (container) => {
+  const temple = document.querySelector('#picture').content;
+  const element = temple.querySelector('.picture');
 
+  const galleryResponse = getGalleryFromServer();
 
+  galleryResponse
+    .then(
+      (response) => {
+        if (!response.ok) {
+          throw `Error. Response status: ${response.status}`;
+        }
+        response.json().then(
+          (pictures) => {
+            for (const picture of pictures) {
+              const newPicture = createPictureElement(picture, element);
+              container.appendChild(newPicture);
+            }
+          }
+        );
+      }
+    )
+    .catch(
+      (error) => {
+        showError(error, 'Ошибка получения данных с сервера');
+      }
+    );
+};
+
+const createPictureElement = (data, element) => {
   const clone = element.cloneNode(true);
-
   const image = clone.querySelector('.picture__img');
   const likes = clone.querySelector('.picture__likes');
   const comments = clone.querySelector('.picture__comments');
@@ -14,16 +41,4 @@ function createPictureElement(data, element) {
   comments.textContent = data.comments;
 
   return clone;
-}
-
-export function fillPicturesList(container) {
-  const temple = document.querySelector('#picture').content;
-  const element = temple.querySelector('.picture');
-
-  const pictures = generatePhotos();
-
-  for (const picture of pictures) {
-    const newPicture = createPictureElement(picture, element);
-    container.appendChild(newPicture);
-  }
-}
+};
--- js/overlay_controller.js
+++ js/overlay_controller.js
@@ -1,33 +1,26 @@
-import {isStringLengthWithinRange} from './util.js';
 import './overlay_const.js';
 import {
-  commentMinLength,
-  commentMaxLength,
-  hashtagRegex,
-  minScale,
-  maxScale,
-  scaleStep,
-  errorMessages,
-  effectsSettings
+  MIN_SCALE,
+  MAX_SCALE,
+  SCALE_STEP,
+  EFFECTS_SETTINGS
 } from './overlay_const.js';
 
 export const overlay = document.querySelector('.img-upload__overlay');
 export const mainForm = document.querySelector('.img-upload__form');
 
-const commentTextarea = document.querySelector('.text__description');
-const hashtagTextarea = document.querySelector('.text__hashtags');
 const slider = document.querySelector('.effect-level__slider');
 const scaleControllerValue = document.querySelector('.scale__control--value');
 const preview = document.querySelector('.img-upload__preview').querySelector('img');
 const effectLevelValue = document.querySelector('.effect-level__value');
 
-export function applyEffectOnPreview() {
+export const applyEffectOnPreview = () => {
   const checkedRadio = document.querySelector('input[name="effect"]:checked');
   const effectClass = checkedRadio.parentElement.querySelector('.effects__preview').classList[1];
   preview.classList = [];
   preview.classList.add(effectClass);
 
-  const effectSettings = effectsSettings[checkedRadio.id];
+  const effectSettings = EFFECTS_SETTINGS[checkedRadio.id];
   if (slider.noUiSlider) {
     slider.noUiSlider.destroy();
   }
@@ -42,68 +35,67 @@ export function applyEffectOnPreview() {
   } else {
     preview.style.filter = null;
   }
-}
+};
 
-export function showError(titleText, buttonText) {
-  const temple = document.querySelector('#error').content;
-  const element = temple.querySelector('.error').cloneNode(true);
-
-  const title = element.querySelector('.error__title');
-  const button = element.querySelector('.error__button');
-  title.textContent = titleText;
-  button.textContent = buttonText;
-
-  button.addEventListener('click', () => {clickErrorButton(element);});
-
-  document.body.appendChild(element);
-
-}
-
-export function changePreviewScale(isIncrease) {
+export const changePreviewScale = (isIncrease) => {
   const currentValue = Math.floor(parseFloat(scaleControllerValue.value));
   let scale = currentValue;
   if (isIncrease) {
-    if (currentValue >= minScale && currentValue < maxScale) {
-      scale = currentValue + scaleStep;
+    if (currentValue >= MIN_SCALE && currentValue < MAX_SCALE) {
+      scale = currentValue + SCALE_STEP;
     }
   } else {
-    if (currentValue > minScale && currentValue <= maxScale) {
-      scale = currentValue - scaleStep;
+    if (currentValue > MIN_SCALE && currentValue <= MAX_SCALE) {
+      scale = currentValue - SCALE_STEP;
     }
   }
   scaleControllerValue.value = `${scale}%`;
   preview.style.transform = `scale(${scale/100})`;
-}
+};
 
-export function getErrorMessage() {
-  let message = 'succes';
-  if (!isHashtagCorrect()) {message = 'hashtag';}
-  else if (!isCommentCorrect()) {message = 'comment';}
-  //часть про файл потом
-  return errorMessages[message];
-}
+export const showSuccesMessage = () => {
+  const temple = document.querySelector('#success').content;
+  const element = temple.querySelector('.success').cloneNode(true);
 
-export function showOverlay() {
+  const button = element.querySelector('.success__button');
+
+  button.addEventListener('click', () => {element.remove();});
+
+  document.body.appendChild(element);
+};
+
+export const showOverlay = () => {
   overlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-}
+};
 
-export function hideOverlay() {
+export const showOverlayWithImage = (imageUrl) => {
+  preview.src = imageUrl;
+  showOverlay();
+};
+
+export const hideOverlay = () => {
   overlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
+};
+
+export const hideOverlayAndResetForm = () => {
+  hideOverlay();
+  resetForm();
+};
+
+const resetForm = () => {
   mainForm.reset();
-}
+  preview.classList = [];
+  preview.style.filter = null;
+  preview.style.transform = null;
+  if (slider.noUiSlider) {
+    slider.noUiSlider.destroy();
+  }
 
-export function isFormCorrect() {
-  return isHashtagCorrect() && isCommentCorrect();
-}
+};
 
-function clickErrorButton(error) {
-  error.remove();
-  overlay.classList.remove('hidden');
-}
-
-function createSliderForEffect(effectSettings) {
+const createSliderForEffect = (effectSettings) => {
   noUiSlider.create(slider, {
     start: effectSettings.max,
     connect: 'lower',
@@ -118,14 +110,4 @@ function createSliderForEffect(effectSettings) {
       }
     }
   });
-}
-
-function isCommentCorrect() {
-  const comment = commentTextarea.value;
-  return isStringLengthWithinRange(comment, commentMinLength, commentMaxLength);
-}
-
-function isHashtagCorrect() {
-  const hashtag = hashtagTextarea.value;
-  return hashtagRegex.test(hashtag);
-}
+};
